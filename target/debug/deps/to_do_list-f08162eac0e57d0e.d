b5a778a79dd98a91aae0f4bc5351f0bdff6dbf28
John <john.oladeji@slsbank.com>
2024-01-13 13:55:44 +0100

    implemented to do list

---
+++ target/debug/deps/to_do_list-f08162eac0e57d0e.d
@@ -0,0 +1,5 @@
+/Users/user/Documents/rustCode/to_do_list/target/debug/deps/to_do_list-f08162eac0e57d0e: src/main.rs
+
+/Users/user/Documents/rustCode/to_do_list/target/debug/deps/to_do_list-f08162eac0e57d0e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/to_do_list.d
@@ -0,0 +1,1 @@
+/Users/user/Documents/rustCode/to_do_list/target/debug/to_do_list: /Users/user/Documents/rustCode/to_do_list/src/main.rs
